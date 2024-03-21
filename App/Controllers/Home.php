104d8e524ab24180aaf329eca22b2df30354da78
tien <tienphan09098@gmail.com>
2024-03-21 15:01:51 +0700

    layout home

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -1,7 +1,13 @@
 <?php
-     class Home{
-          public function hello(){
-               echo '<p style="text-align:center;">こんにちわ！</p>';
+     class Home extends Controller{
+          public $productModel;
+
+          public function __construct(){
+               $this->productModel = $this->model('ProductModel');
+          }
+          public function index(){
+               $sanPhams = $this->productModel->get();
+               $this->view('/Client/Home',$sanPhams);
           }
      }
 ?>
---
+++ App/Views/Client/layouts/header.php
@@ -0,0 +1,24 @@
+<!------------------------------------------------------Sticky header---------------------------------------------------------------->
+<nav class="navbar sticky-top navbar-expand-lg " style="background: linear-gradient(to right, #ffad3d, #fb8d17)">
+    <div class="container-fluid px-5 py-2">
+        <div class="col-md-6 col-sm-12 justify-content-small-center">
+            <a href="#"><img src="./resources/images/header-logo.png" style="width: 10%;"></a>
+        </div>
+        <div class="col-md-6 col-sm-12">
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link mx-2" href="#!"><i class="fa-solid fa-house icon"></i>Trang chủ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mx-2" href="#!"><i class="fa-solid fa-list icon"></i>Danh mục</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mx-2" href="#!"><i class="fa-brands fa-shopify icon"></i>Sản phẩm</a>
+                </li>
+                <li class="nav-item ms-3">
+                    <a class="btn btn-black btn-rounded" href="#" style="border: 2px solid black;"><i class="fa-solid fa-user icon"></i>Đăng nhập</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
