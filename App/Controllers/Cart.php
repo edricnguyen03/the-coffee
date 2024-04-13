bdd516bbffcdc76a97b65e7b8617f14ce3bc16de
trongphu567 <trongphu543210@gmail.com>
2024-04-13 22:16:06 +0700

    update cart controller

--- App/Controllers/Cart.php
+++ App/Controllers/Cart.php
@@ -1,13 +1,17 @@
 <?php
-class Cart extends Controller{
+class Cart extends Controller
+{
      public $cartModel;
      public $data;
 
-     public function __construct(){
+     public function __construct()
+     {
           $this->cartModel = $this->model('CartModel');
           $this->data = [];
      }
 
-     
+     public function index()
+     {
+          $this->view('/Client/Cart', $this->data);
+     }
 }
-?>
