b3536c6fd1e3af5c58d6137d48a3d45ca9fb29b7
tien <tienphan09098@gmail.com>
2024-05-14 22:20:11 +0700

    validate product

--- App/Views/Admin/pages/products/create.php
+++ App/Views/Admin/pages/products/create.php
@@ -74,7 +74,7 @@ require_once('./App/Views/Admin/layouts/header.php');
                         </div>
                         <div class=" mb-3">
                             <label for="description" class="form-label">Miêu tả</label>
-                            <input type="text" class="form-control" id=" description" name="description" required>
+                            <input type="text" class="form-control" id="description" name="description" required>
                         </div>
                         <button type="submit" id="submit" name="submit" class="btn btn-primary">Tạo sản phẩm</button>
                     </form>
@@ -108,11 +108,11 @@ require_once('./App/Views/Admin/layouts/header.php');
     var txtDescription = document.getElementById('description');
 
     function validate(){
-        if(txtProductName.value == '' || txtPrice.value == '' || txtWeight.value == '' || txtContent.value == '' || txtDescription.value == '') {
+        if(txtProductName.value.trim() == '' || txtPrice.value.trim() == '' || txtWeight.value.trim() == '' || txtContent.value.trim() == '' || txtDescription.value.trim() == '') {
             document.getElementById('error').innerHTML = '<div class="alert alert-danger text-center" role="alert"> Không để trống các ô</div>';
             return false;
         }
-        if (!/^[a-zA-ZÀ-ỹ0-9\s]{4,40}$/.test(txtProductName.value)) {
+        if (!/^[a-zA-ZÀ-ỹ0-9\s]{4,40}$/.test(txtProductName.value.trim())) {
             document.getElementById('error').innerHTML = '<div class="alert alert-danger text-center" role="alert"> Tên không hợp lệ từ 4 đến 40 kí tự chữ cái và số</div>';
             return false;
         }
@@ -134,11 +134,11 @@ require_once('./App/Views/Admin/layouts/header.php');
             document.getElementById('error').innerHTML = '<div class="alert alert-danger text-center" role="alert"> Cân nặng không hợp lệ từ 1 đến 10 kí tự số</div>';
             return false;
         }
-        if (!/^[a-zA-ZÀ-ỹ0-9\s]{4,40}$/.test(txtContent.value)) {
+        if (!/^[a-zA-ZÀ-ỹ0-9\s]{4,40}$/.test(txtContent.value.trim())) {
             document.getElementById('error').innerHTML = '<div class="alert alert-danger text-center" role="alert"> Nội dung không hợp lệ từ 4 đến 40 kí tự chữ cái và số</div>';
             return false;
         }
-        if (!/^[a-zA-ZÀ-ỹ0-9\s]{4,40}$/.test(txtDescription.value)) {
+        if (!/^[a-zA-ZÀ-ỹ0-9\s]{4,40}$/.test(txtDescription.value.trim())) {
             document.getElementById('error').innerHTML = '<div class="alert alert-danger text-center" role="alert"> Mô tả không hợp lệ từ 4 đến 40 kí tự chữ cái và số</div>';
             return false;
         }
